6c6aedebdbcabb4b4a35fdbc5708f3c0c0086bd4
harishpuvvada <hp1047@nyu.edu>
2018-05-29 19:55:03 -0400

    update delete added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect( 'mongodb://localhost:27017/TodoApp');
+mongoose.connect( 'mongodb://root:root@ds137720.mlab.com:37720/harish-todo-api-db' ||'mongodb://localhost:27017/TodoApp');
 
-//'mongodb://root:root@ds137720.mlab.com:37720/harish-todo-api-db' ||
+//
 // For mongoose configuration
 module.exports ={
 	mongoose : mongoose
