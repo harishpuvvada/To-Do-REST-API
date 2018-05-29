9ca9182470f55c0f9fc1ea5364436c4adf2e6fa3
harishpuvvada <hp1047@nyu.edu>
2018-05-28 22:02:21 -0400

    add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,46 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+//var id = '5b0ca9ca7f008347059af41b11';
+var id = '5b0b6c4cad9abe9864401d6a'
+// if(!ObjectID.isValid(id)){
+// 	console.log('ID not valid');
+// }
+
+
+// Todo.find({
+// 	_id:id
+// }).then((todos)=>{
+
+// console.log('Todos',todos)
+
+// });
+
+// Todo.findOne({
+// 	_id:id
+// }).then((todo)=>{
+
+// console.log('Todo',todo)
+
+// });
+
+// Todo.findById(id).then((todo)=>{
+	
+// 	if(!todo){
+// 		return console.log("ID not found")
+// 	}
+// 	console.log('Todo by ID',todo);
+
+// }).catch((e) => console.log(e));
+
+User.findById(id).then((user)=>{
+	
+	if(!user){
+		return console.log("user not found")
+	}
+	console.log('User by ID',user);
+
+}).catch((e) => console.log(e));
