34cb7074a05c096223c32326ce9c53fb63198090
harishpuvvada <hp1047@nyu.edu>
2018-05-29 18:04:07 -0400

    port number update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds137720.mlab.com:37720/harish-todo-api-db' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://root:root@ds137720.mlab.com:37720/harish-todo-api-db' || 'mongodb://localhost:27017/TodoApp');
 
 
 // For mongoose configuration
