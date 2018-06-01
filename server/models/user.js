b25d5e41dba609e6f9f3b4922ac6f6b3c7613613
harishpuvvada <hp1047@nyu.edu>
2018-05-31 23:02:28 -0400

    hash user pswd

--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,129 @@
 var mongoose = require('mongoose');
+var validator = require('validator');
+var _ = require('lodash');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 
-var User = mongoose.model('User',{
+var UserSchema = new mongoose.Schema({
 
 	email:{
 
 		type: String,
 		required : true,
 		minlength : 1,
-		trim : true
+		trim : true,
+		unique : true,
+		validate : {
+				validator: validator.isEmail,
+				message: `is not a valid email`
+			}
+		},
+
+	password : {
+		type : String,
+		required :true,
+		minlength : 6
+	},
+
+	tokens : [{
+
+		access : {
+			type : String,
+			required:true
+		},
+
+		token:{
+			type : String,
+			required:true
+		}
+
+	}]
+
+});
+
+
+//to hide somethings from user we will override a method
+UserSchema.methods.toJSON = function(){
+
+	var user = this;
+	var userObject = user.toObject();
+
+	return _.pick(userObject, ['_id', 'email']); //leaving tokens, password
+
+};
+
+
+
+UserSchema.methods.generateAuthToken = function(){
+
+	var user = this;
+	var access = 'auth';
+	var token = jwt.sign({_id: user._id.toHexString(), access},'abc123').toString();
+
+	user.tokens = user.tokens.concat([{access,token}]);
+
+	return user.save().then(()=>{
+		return token;
+	});
+
+};
+
+UserSchema.statics.findByToken = function (token) {
+	var User = this;
+	var decoded;
+
+	try {
+
+	decoded = jwt.verify(token,'abc123');
+
+
+	}catch (e){
+
+		return new Promise((resolve,reject)=>{
+			reject();
+		});
 
 	}
 
+	return User.findOne({
+
+		'_id': decoded._id,
+		'tokens.token' : token,
+		'tokens.access' : 'auth'
+
+	});
+
+};
+
+UserSchema.pre('save', function(next){
+
+	var user = this;
+
+	if (user.isModified('password')){
+
+		bcrypt.genSalt(10, (err,salt)=>{
+
+			bcrypt.hash(user.password,salt,(err,hash)=>{
+
+				user.password = hash;
+				next();
+
+			});
+
+		})
+
+
+	} else{
+
+		next();
+	}
+
+
 });
+
+
+var User = mongoose.model('User', UserSchema);
 
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose'); //for mongoose config
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
+
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -127,6 +129,31 @@ app.patch('/todos/:id',(req,res)=>{
 		res.status(400).send();
 	});
 	
+
+});
+
+// POST /users
+app.post('/users',(req,res)=>{
+
+	var body = _.pick(req.body, ['email','password']); //subset of things user sent to us
+	var user = new User(body);
+
+	user.save().then((user)=>{
+		return user.generateAuthToken();
+	}).then((token)=>{
+		res.header('x-auth',token).send(user);
+	}).catch((e)=>{
+		res.status(400).send(e);
+	});
+
+});
+
+
+
+
+app.get('/users/me',authenticate, (req,res)=>{
+
+		res.send(req.user);
 
 });
 
