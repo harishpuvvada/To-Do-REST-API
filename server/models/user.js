5ccb3d210de119df4bdf7bda693f0da5976daed3
harishpuvvada <hp1047@nyu.edu>
2018-06-02 23:11:49 -0400

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -96,6 +96,33 @@ UserSchema.statics.findByToken = function (token) {
 
 };
 
+UserSchema.statics.findByCredentials = function (email,password){
+
+	var User = this;
+
+	return User.findOne({email}).then((user)=>{
+
+		if(!user){
+			return Promise.reject();
+		}
+
+		return new Promise((resolve,reject)=>{
+
+				//compare password and user.password
+				bcrypt.compare(password,user.password,(err,res)=>{
+					if(res){
+						resolve(user);
+					} else{
+						reject();
+					}
+				});
+		});
+
+	});
+
+};
+
+
 UserSchema.pre('save', function(next){
 
 	var user = this;
@@ -121,6 +148,8 @@ UserSchema.pre('save', function(next){
 
 
 });
+
+
 
 
 var User = mongoose.model('User', UserSchema);
